f856dfcf4323ab64c77d86f0296cf474c0526258
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 11:54:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <div>
+        <button>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+      </div>
+      <h2>Stats</h2>
+      <dl>
+        <dt>good</dt>
+        <dd>6</dd>
+        <dt>neutral</dt>
+        <dd>2</dd>
+        <dt>bad</dt>
+        <dd>1</dd>
+      </dl>
+    </>
+  )
+}
+
+export default App
